6f5d9b273f8b14add76936aa75fc314e9d6d37f1
Jean-CODE-Van-Damme <oreverseau@live.fr>
2022-08-26 19:44:25 +0200

    final VS

--- middleware/isAuthentificated.js
+++ middleware/isAuthentificated.js
@@ -3,17 +3,15 @@
 const User = require("../models/User");
 
 const isAuthentificated = async (req, res, next) => {
-  console.log("YOYO", req.headers);
   if (req.headers.authorization) {
     // Creation d un token avec BearerToken recu du Front
     const tokenPost = req.headers.authorization.replace("Bearer ", "");
-    console.log(tokenPost, "tamere", req.headers.authorization);
+
     // Recherche d'un user avec le meme Token en BDD
     const userWithToken = await User.findOne({ token: tokenPost });
 
     if (userWithToken) {
-      //   req.userWithToken = userWithToken;
-      // on pourra retrouver ce user en cas de besoin avec la clef " req.userWithToken" que l on vient de rajouter
+      // on pourra retrouver ce user en cas de besoin avec la clef " req.User " que l on vient de rajouter
       // a l objet req
       req.User = userWithToken;
       // Si ok, on peut passer a la suite de les routes avec en parametre IsAuthentificated
